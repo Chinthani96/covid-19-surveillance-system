36599c0c8d2d2ea4bbd65c25191bb9f8c7d42f62
Chinthani <chinthani96@gmail.com>
2020-06-05 10:37:32 +0530

    updated DBConnection.java

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -13,7 +13,7 @@ public class DBConnection {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             try {
-                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/covid19_Info","root","modaDinindu1");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/covid19_Info","root","");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
